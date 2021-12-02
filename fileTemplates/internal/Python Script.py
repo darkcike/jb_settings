3d64f70bffa416c1148bb0f7cdb7af8d54034f5d
Jeff Lau <752074518@qq.com>
2021-12-03 01:23:43 +0800

    PC-2021.3 <duzi@DESKTOP-7EH6KBB Create vim_settings.xml, ide.general.xml, git.xml, ide-features-trainer.xml, git_toolbox_2.xml, project.default.xml, diff.xml, Twilight copy.icls, shared-indexes.xml, Default.xml, ignore.xml, find.xml, filetypes.xml, Python Script.py, plugin_ui.xml, colors.scheme.xml, KeyPromoterXSettings.xml, notifications.xml, _@user_IntelliJ Light.icls, laf.xml, customization.xml, Default.xml, jdk.table.xml, dbnavigator.xml, vcs.xml, grazie_global.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Python Script.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+==================================================
+@Project -> File :${PROJECT_NAME} -> ${NAME}.py
+@Time   :${DATE} ${TIME}
+@Author :Jeff Lau
+@Desc   :
+==================================================
+"""
